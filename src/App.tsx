78a5bd866f084e1f0bdde2475dadb61f2622c2d6
Taher Chabra <redsign12345@gmail.com>
2024-06-08 22:16:49 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react"
 import { Select, SelectOption } from "./Select"
+import styles from "./css/app.module.css"
 
 const options = [
   {label: "First", value: 1},
   {label: "Second", value: 2},
   {label: "Third", value: 3},
   {label: "Fourth", value: 4},
-  {label: "Fifth", value: 5}
+  {label: "Fifth", value: 5},
+  {label: "Sixth", value: 6},
+  {label: "Seventh", value: 7},
+  {label: "Eighth", value: 8},
+  {label: "Ninth", value: 9},
+  {label: "Tenth", value: 10}
 ]
 
 function App() {
@@ -14,7 +20,7 @@ function App() {
   const [value2, setValue2] = useState<SelectOption | undefined>(options[0])
 
   return (
-    <div>
+    <div className={styles.select}>
       <Select 
       multiple
       options={options} 
